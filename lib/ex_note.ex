f1c33b4d27c27acef6b5c50a1b7bc6f3cbfaa16c
Trevor Brown <tbrown@voalte.com>
2014-07-27 18:58:10 -0400

    Added primary functions in the public interface.

--- lib/ex_note.ex
+++ lib/ex_note.ex
@@ -1,4 +1,14 @@
 defmodule ExNote do
-  def get_notes() do
+  def get_notes(project, version, options) do
+  end
+
+  def get_todos(project, version, options) do
+    todo_options = options # modify options
+    get_notes(project, version, todo_options)
+  end
+
+  def get_fixmes(project, version, options) do
+    fixme_options = options # modify options
+    get_notes(project, version, fixme_options)
   end
 end
