4b80633c8957f3c691903f5cc5360db08a545adb
Trevor Brown <tbrown@voalte.com>
2014-08-14 09:21:04 -0400

    Created Mix.Tasks.Exnote module for utility tasks.

---
+++ lib/mix/tasks/exnote.ex
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.Exnote do
+  use Mix.Task
+
+  @shortdoc "Handles custom ExUnit setup/teardown logic"
+  @recursive true
+  @moduledoc """
+  Handles custom setup and teardown logic for ExUnit tests. Normally run before
+  and after test runs.
+  """
+
+  @doc """
+  List ExNoteTest tasks with help
+  """
+  def run([]) do
+    help
+  end
+
+  def run(["--help"]) do
+    help
+  end
+
+  def help do
+    Mix.shell.info """
+    Handles custom setup and teardown logic for ExUnit tests. Normally run before and after test runs.
+
+    Commands:
+    mix exnote.setup    # Compile modules used for testing with ExUnit
+    mix exnote.setup    # Cleanup compiled modules
+    """
+  end
+end
